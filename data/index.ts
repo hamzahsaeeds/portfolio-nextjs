9f2bd957bf48467b42bf7d696ecacf7aa24bb725
hamzahsaeeds <hamzahsaeed23@hotmail.com>
2024-08-18 20:54:50 +0500

    updated work experience

--- data/index.ts
+++ data/index.ts
@@ -173,29 +173,29 @@ export const companies = [
 export const workExperience = [
   {
     id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    title: "Frontend Developer Intern",
+    desc: "Assisted in the development of a desktop-based platform using React.js and Electron.js, enhancing interactivity and user experience.",
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    title: "Frontend Developer - FarmersDirect",
+    desc: "Worked on transforming activity feeds (supply chain to show customers of the entire chain coffee goes through) web app using NextJS, Typescript, ReactJS, TailwindCSS, Shadcn UI, GraphQL, Turbo, Google Maps, Opengraph, and PNPM.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp2.svg",
   },
   {
     id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    title: "Freelance App Dev Projects",
+    desc: "Led development of several apps for clients, from initial concept to deployment on app stores.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp3.svg",
   },
   {
     id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    title: "Full Stack Developer",
+    desc: "Developed and maintained user-facing features using modern tools and technologies.",
     className: "md:col-span-2",
     thumbnail: "/exp4.svg",
   },

3a2c4c54d61461bf9dbd2b0cf8d420da8126370b
hamzahsaeeds <hamzahsaeed23@hotmail.com>
2024-08-18 16:20:12 +0500

    name change and email address

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -67,7 +67,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "hamzahsaeed23@hotmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
